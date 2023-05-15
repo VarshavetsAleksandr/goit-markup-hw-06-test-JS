0cda9be46f92728c13f09bb194feca83e30dd692
VarshavetsAleksandr <varshavetsaleksandr@gmail.com>
2023-05-16 00:59:19 +0300

    1

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-const formEl = document.querySelector('[name="modal_form"]');
+const formEl = document.querySelector('[name="modal_phone"]');
 const phoneEl = document.querySelector('.modal-form-input[type="tel"]');
 const formIconEl = document.querySelector('[data-form-icon]');
 const formFieldEl = document.querySelector('[data-form-field]');
@@ -9,10 +9,13 @@ Inputmask('+38 (0**) 999-99-99', {
   oncomplete: () => {
     formIconEl.classList.add('valid-phone');
     formFieldEl.classList.add('valid-field');
+    formIconEl.classList.remove('invalid-phone');
+    formFieldEl.classList.remove('invalid-field');
   },
   oncleared: () => {
     formIconEl.classList.remove('valid-phone');
     formFieldEl.classList.remove('valid-field');
+    formIconEl.classList.add('invalid-phone');
+    formFieldEl.classList.add('invalid-field');
   },
 }).mask(phoneEl);
-
